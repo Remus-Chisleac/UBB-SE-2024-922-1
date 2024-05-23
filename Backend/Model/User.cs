580cd70c4698a6fe082fc4e738a6c46d132b7816
Cseke <stefan.cseke@stud.ubbcluj.ro>
2024-05-23 14:21:30 +0300

    modified user for json deserialization

--- Backend/Model/User.cs
+++ Backend/Model/User.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Moderation.Entities
 {
     public class User : IHasID
     {
+        [JsonPropertyName("guid")]
         public Guid Id { get; set; }
+        [JsonPropertyName("name")]
         public string Username { get; set; }
+        [JsonPropertyName("password")]
         public string Password { get; set; }
+
+        public User()
+        {
+        }
 
         public User(string username)
         {
